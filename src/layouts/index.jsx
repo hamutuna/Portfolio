f4ab1ffb38a7750e2e8ccc0579907dd052be5362
tanishi109 <note109@gmail.com>
2018-04-02 21:34:20 +0900

    Header & Footer component moved to layouts/index.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,21 @@
 // @flow
 import * as React from 'react';
 
+import Header from '../components/index/Header';
+import Footer from '../components/index/Footer';
+
 import '../styles/global';
 
 type Props = {
-  children: () => React.Element<any>
+  children: () => React.Element<any>,
 };
 
-export default (props: Props) => <div>{props.children()}</div>;
+export default (props: Props) => {
+  return (
+    <React.Fragment>
+      <Header />
+      <main>{props.children()}</main>
+      <Footer />
+    </React.Fragment>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,16 @@
 // @flow
 import * as React from 'react';
 
-import Header from '../components/index/Header';
 import FirstView from '../components/index/FirstView';
 import Profile from '../components/index/Profile';
 import Skills from '../components/index/Skills/Skills';
 import Works from '../components/index/Works/Works';
-import Footer from '../components/index/Footer';
 
 export default () => (
   <React.Fragment>
-    <Header />
-    <main>
-      <FirstView />
-      <Profile />
-      <Skills />
-      <Works />
-    </main>
-    <Footer />
+    <FirstView />
+    <Profile />
+    <Skills />
+    <Works />
   </React.Fragment>
 );
