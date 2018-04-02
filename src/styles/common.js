ed63ea6114e20cc1d60305984dc0fe5047d892eb
tanishi109 <note109@gmail.com>
2018-04-02 15:07:52 +0900

    Profile styled roughly.

---
+++ src/styles/common.js
@@ -0,0 +1,16 @@
+// @flow
+import { css } from 'styled-components';
+
+export const column = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const row = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
--- src/styles/index/Profile.js
+++ src/styles/index/Profile.js
@@ -1,10 +1,43 @@
 // @flow
 import styled from 'styled-components';
 
-const Title = styled.h1`
+import { column, row } from '../../styles/common';
+
+export const Section = styled.section`
+  ${column};
+  padding: 0 34px;
+`;
+
+export const Title = styled.h1`
   margin-top: 92px;
 `;
 
-export default {
-  Title,
-};
+export const Image = styled.img`
+  background-color lightgray;
+  width: 212px;
+  height: 212px;
+  border-radius: 50%;
+`;
+
+export const IconWrapper = styled.div`
+  ${row};
+  margin-top: 42px;
+`;
+
+export const Icon = styled.img`
+  background-color gray;
+  width: 72px;
+  height: 72px;
+
+  &:nth-child(1) {
+    margin-right: 15px;
+  }
+
+  &:nth-child(2) {
+    margin-left: 15px;
+  }
+`;
+
+export const Description = styled.p`
+  margin-top: 66px;
+`;
