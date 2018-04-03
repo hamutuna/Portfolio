c5fc8c1da1520574f88d7cbd5218230603161fe0
tanishi109 <note109@gmail.com>
2018-04-03 19:32:18 +0900

    global var

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb',
   plugins: ['flowtype'],
+  globals: {
+    graphql: false,
+  },
   rules: {
     'arrow-parens': ['error', 'always'],
     'object-curly-newline': ['error', 'never'],
