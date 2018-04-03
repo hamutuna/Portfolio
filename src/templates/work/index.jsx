2b6cc0a3ca90eeb5c685e36cda2d4d620f0de37c
tanishi109 <note109@gmail.com>
2018-04-03 17:13:17 +0900

    Contents data applied.

--- src/templates/work/index.jsx
+++ src/templates/work/index.jsx
@@ -1,15 +1,33 @@
 // @flow
 import * as React from 'react';
 
+type Work = {
+  id: string,
+  title: string,
+  position: string,
+  skills: [string],
+  ingenuity: string,
+};
+
 type Props = {
   pageContext: {
-    id: string,
+    work: Work,
   },
 };
 
 export default (props: Props) => {
   const { pageContext } = props;
-  const nth = pageContext.id;
+  const { id, title, position, skills, ingenuity } = pageContext.work;
 
-  return <div>work {nth} page!!!</div>;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>担当範囲</h2>
+      <p>{position}</p>
+      <h2>使用skill</h2>
+      <p>{skills}</p>
+      <h2>工夫した点</h2>
+      <p>{ingenuity}</p>
+    </div>
+  );
 };
