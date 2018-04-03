76a7199fc82379b31f9c996ea5e9a9d4ef301ffd
tanishi109 <note109@gmail.com>
2018-04-03 19:29:34 +0900

    Change rules to adjust to prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb',
   plugins: ['flowtype'],
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'object-curly-newline': ['error', 'never'],
+  },
 };
