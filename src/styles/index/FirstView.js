1fd9f27e836131d46400ee836be9a0c6ce1b2e34
tanishi109 <note109@gmail.com>
2018-04-06 01:57:00 +0900

    Fix var

--- src/styles/index/FirstView.js
+++ src/styles/index/FirstView.js
@@ -5,6 +5,6 @@ import { headerHeight, underFirstView } from '../../styles/var';
 
 export const FirstView = styled.div`
   width: 100v;
-  height: calc(100vh - headerHeight - ${underFirstView});
+  height: calc(100vh - ${headerHeight} - ${underFirstView});
   background-color: lightgray;
 `;
