75f9478b4fb2f3506cf575cf87896821d1870cc3
tanishi109 <twitanishi@gmail.com>
2019-02-10 02:41:44 +0900

    Layout added for work page.

--- src/templates/work/index.jsx
+++ src/templates/work/index.jsx
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby';
 import type { Work } from '../../entities/types';
 import * as s from '../../styles/templates/work/index';
 
+import Layout from '../../components/_common/Layout';
 import { anchors } from '../../components/_common/NaviItem';
 
 type Props = {
@@ -18,45 +19,63 @@ const getDocImages = (documents, json) => documents.map((d) => json[d]);
 
 export default (props: Props) => {
   const { pageContext, data } = props;
-  const {
-    id,
-    title,
-    description,
-    position,
-    skills,
-    detail,
-    documents,
-  } = pageContext.work;
+  const { id, title, description, position, skills, detail, documents } = pageContext.work;
+  const { pageTopImage, hamburgerImage, closeImage, logoImage } = data;
   const image = data[`${id}Image`];
   const docImages = getDocImages(documents, data);
 
   return (
-    <s.Wrapper>
-      <s.FirstView image={image}>
-        <s.TopImage resolutions={image.resolutions} />
-      </s.FirstView>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <h2 id={anchors.position}>担当工程/役割</h2>
-      <p dangerouslySetInnerHTML={{ __html: position }} />
-      <h2>使用skill</h2>
-      {skills.map((skill) => <p key={skill.id}>{skill.title}</p>)}
-      <h2 id={anchors.detail}>詳細</h2>
-      <p dangerouslySetInnerHTML={{ __html: detail }} />
-      <h2 id={anchors.documents}>実際の資料</h2>
-      {docImages.map((d, i) => (
-        <s.DocImage
-          resolutions={d.resolutions}
-          alt={`document${i}`}
-          key={d.resolutions.src}
+    <Layout
+      pageTopImage={pageTopImage}
+      hamburgerImage={hamburgerImage}
+      closeImage={closeImage}
+      logoImage={logoImage}
+    >
+      <s.Wrapper>
+        <s.FirstView image={image}>
+          <s.TopImage resolutions={image.resolutions} />
+        </s.FirstView>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <h2 id={anchors.position}>担当工程/役割</h2>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: position,
+          }}
         />
-      ))}
-    </s.Wrapper>
+        <h2>使用skill</h2>
+        {skills.map((skill) => (
+          <p key={skill.id}>{skill.title}</p>
+        ))}
+        <h2 id={anchors.detail}>詳細</h2>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: detail,
+          }}
+        />
+        <h2 id={anchors.documents}>実際の資料</h2>
+        {docImages.map((d, i) => (
+          <s.DocImage resolutions={d.resolutions} alt={`document${i}`} key={d.resolutions.src} />
+        ))}
+      </s.Wrapper>
+    </Layout>
   );
 };
 
 export const query = graphql`
   query GetWorkContents {
+    logoImage: imageSharp(resolutions: { originalName: { eq: "logo.png" } }) {
+      ...ImgFragment
+    }
+    pageTopImage: imageSharp(resolutions: { originalName: { eq: "page_top.png" } }) {
+      ...ImgFragment
+    }
+    hamburgerImage: imageSharp(resolutions: { originalName: { eq: "hamburger.png" } }) {
+      ...ImgFragment
+    }
+    closeImage: imageSharp(resolutions: { originalName: { eq: "close.png" } }) {
+      ...ImgFragment
+    }
     project1Image: imageSharp(resolutions: { originalName: { eq: "work_project1.png" } }) {
       ...ImgFragment
     }
