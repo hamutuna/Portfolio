43a65a979dd4ec2ce999b3f74259cc27aa4b8890
tanishi109 <twitanishi@gmail.com>
2019-02-28 16:09:49 +0900

    :recycle: Use async await.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,41 @@
 const path = require('path');
 
-exports.createPages = ({ graphql, actions }) => new Promise((resolve, reject) => {
-  graphql(`
-      {
-        allWorksJson {
-          edges {
-            node {
+exports.createPages = async ({ graphql, actions }) => {
+  const result = await graphql(`
+    {
+      allWorksJson {
+        edges {
+          node {
+            id
+            title
+            position
+            positionAndDate
+            description
+            skills {
               id
               title
-              position
-              positionAndDate
-              description
-              skills {
-                id
-                title
-              }
-              detail
-              documents
-              goodPoints {
-                id
-                title
-                description
-              }
-              members
             }
+            detail
+            documents
+            goodPoints {
+              id
+              title
+              description
+            }
+            members
           }
         }
       }
-    `).then((result) => {
-    const { createPage } = actions;
-    result.data.allWorksJson.edges.forEach(({ node }) => {
-      createPage({
-        path: `works/${node.id}`,
-        component: path.resolve('./src/templates/work/index.jsx'),
-        context: {
-          work: node,
-        },
-      });
+    }
+  `);
+  const { createPage } = actions;
+  result.data.allWorksJson.edges.forEach(({ node }) => {
+    createPage({
+      path: `works/${node.id}`,
+      component: path.resolve('./src/templates/work/index.jsx'),
+      context: {
+        work: node,
+      },
     });
-    resolve();
   });
-});
+};
